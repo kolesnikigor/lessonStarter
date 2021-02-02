067cd22fd81e205903c2435631e010d4b03a0731
KolesnikIgor <kiv7077@gmail.com>
2021-02-02 15:27:17 +0200

    fixed some remarks

--- src/scripts/components/renderBar/renderBar.js
+++ src/scripts/components/renderBar/renderBar.js
@@ -6,32 +6,23 @@ const renderBar = ({
   const calendarBarTitle = document.createElement("span");
   const btnPrevMonth = document.createElement('button');
   const btnNextMonth = document.createElement('button');
+
   calendarToolbar.classList.add("calendar-bar");
   calendarBarTitle.classList.add("calendar-bar__title");
   btnPrevMonth.classList.add("calendar-bar__btn-prev");
   btnNextMonth.classList.add("calendar-bar__btn-next");
+
   btnPrevMonth.innerHTML = "←";
   btnNextMonth.innerHTML = "→";
-
-  console.log(btnNextMonth, btnPrevMonth); // Add class example
-  // here toolBar content rendering
   appElement.prepend(calendarToolbar);
   calendarToolbar.append(calendarBarTitle);
   calendarBarTitle.before(btnPrevMonth);
   calendarBarTitle.after(btnNextMonth);
-  let DateForShow = currentDate;
-  let getYear = DateForShow.getFullYear(); //2017
-  let getMonth = DateForShow.toLocaleDateString('en-US', {
-    month: 'long',
-  });
-  console.log(DateForShow);
-  console.log(getYear, getMonth);
-  // let currentMonth = new Date().toLocaleDateString('en-US', options);
-  calendarBarTitle.innerHTML = getMonth + " " + getYear;
 
-  // btnNextMonth.addEventListener('click' () => {
-  //   currentMonth = new Date()
-  // });
+  calendarBarTitle.innerHTML = currentDate.toLocaleDateString('en-US', {
+      month: 'long',
+      year: 'numeric'
+    });
 };
 
 export default renderBar;
